c9f7dd69ed93ea406503988424197ee84f5e6035
d.doroshenko <d.doroshenko@mt-innovations.ru>
2021-06-06 00:14:36 +0300

    did layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'mf3-cards',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mf3-cards';
 }
