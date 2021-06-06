c9ee8551d83d59fee43a0fba5ec4eb7c14396707
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2021-06-06 03:57:00 +0300

    Update publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ sharedMappings.register(
 module.exports = {
   output: {
     uniqueName: "mf3Cards",
-    publicPath: "auto"
+    publicPath: "/cards/"
   },
   optimization: {
     runtimeChunk: false
