f33c5547f8f8fd7147c57c42afb8b287b92b82a4
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2021-06-06 05:03:59 +0300

    Base href

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ sharedMappings.register(
 module.exports = {
   output: {
     uniqueName: "mf3Cards",
-    publicPath: "/cards/"
+    publicPath: "auto"
   },
   optimization: {
     runtimeChunk: false
